b3b87e55da8d0ec5e1d09153740e8db338aeeb63
Tony <jinniw43805@gmail.com>
2018-10-14 14:58:31 -0500

    First commit

---
+++ node.java
@@ -0,0 +1,32 @@
+package bin;
+public class Node implements java.io.Serializable{
+    private int nodeId;
+    private String host;
+    private String port;
+    private Node root;
+    private Node parent;
+    private Node[] children;
+    
+    public Node(int index, String hostName, String port){
+        this.nodeId = index;
+        this.host = hostName;
+        this.port = port;
+    }
+    
+    public String configToSring(){
+        return String.format("%d %s %s", nodeId, host, port);
+    }
+
+    public int getNodeId(){
+        return nodeId;
+    }
+    
+    public String getHostName(){
+        return host;
+    }
+    
+    public int getPort(){
+        return Integer.parseInt(port);
+    }
+
+}
